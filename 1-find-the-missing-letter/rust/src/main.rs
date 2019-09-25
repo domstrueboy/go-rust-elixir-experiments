cbc4b68849d37255e4bd570e3b483634d5cecbf6
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2019-09-26 00:22:13 +0500

    Cargo project was generated

---
+++ 1-find-the-missing-letter/rust/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+    let letter = missing([]rune{'a', 'b', 'c', 'd', 'f', 'g', 'h'});
+	let letter2 := missing([]rune{'O', 'Q', 'R', 'S'})
+	println!(letter, letter2);
+}
+
+fn missing(input []rune) string {
+	start := int(input[0])
+	for i, j := 0, start; i < 32; i, j = i+1, j+1 {
+		if int(input[i]) != j {
+			return string(j)
+		}
+	}
+	return ""
+}
