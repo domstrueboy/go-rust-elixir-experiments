04e4685c8c735d18b627fb9fe05235a268b94498
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-03-09 16:40:22 +0500

    img converting works

--- 3-img-server/rust/src/main.rs
+++ 3-img-server/rust/src/main.rs
@@ -1,3 +1,18 @@
+extern crate image;
+
+use image::GenericImageView;
+
 fn main() {
-    println!("Hello, world!");
+    // Use the open function to load an image from a Path.
+    // `open` returns a `DynamicImage` on success.
+    let img = image::open("img/input/progressive.jpg").unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
+
+    // Write the contents of this image to the Writer in PNG format.
+    img.save("img/output/progressive.png").unwrap();
 }
