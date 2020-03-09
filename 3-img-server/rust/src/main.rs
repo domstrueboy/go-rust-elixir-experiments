cf16f05b2e896f3eb27998b738032fea98f92c99
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-03-09 21:01:17 +0500

    server is added

--- 3-img-server/rust/src/main.rs
+++ 3-img-server/rust/src/main.rs
@@ -1,21 +1,79 @@
-extern crate image;
+#![deny(warnings)]
 
-use image::GenericImageView;
+use futures_util::TryStreamExt;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
-fn main() {
-    // Use the open function to load an image from a Path.
-    // `open` returns a `DynamicImage` on success.
-    let img = image::open("img/input/progressive.jpg").unwrap();
+/// This is our service handler. It receives a Request, routes on its
+/// path, and returns a Future of a Response.
+async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        // Serve some instructions at /
+        (&Method::GET, "/") => Ok(Response::new(Body::from(
+            "Try POSTing data to /echo such as: `curl localhost:3000/echo -XPOST -d 'hello world'`",
+        ))),
 
-    // The dimensions method returns the images width and height.
-    println!("dimensions {:?}", img.dimensions());
+        // Simply echo the body back to the client.
+        (&Method::POST, "/echo") => Ok(Response::new(req.into_body())),
 
-    // The color method returns the image's `ColorType`.
-    println!("{:?}", img.color());
+        // Convert to uppercase before sending back to client using a stream.
+        (&Method::POST, "/echo/uppercase") => {
+            let chunk_stream = req.into_body().map_ok(|chunk| {
+                chunk
+                    .iter()
+                    .map(|byte| byte.to_ascii_uppercase())
+                    .collect::<Vec<u8>>()
+            });
+            Ok(Response::new(Body::wrap_stream(chunk_stream)))
+        }
 
-    // Write the contents of this image to the Writer in PNG format.
-    img
-    .resize(160, 90, image::imageops::Lanczos3)
-    .save("img/output/progressive.jpg")
-    .unwrap();
+        // Reverse the entire body before sending back to the client.
+        //
+        // Since we don't know the end yet, we can't simply stream
+        // the chunks as they arrive as we did with the above uppercase endpoint.
+        // So here we do `.await` on the future, waiting on concatenating the full body,
+        // then afterwards the content can be reversed. Only then can we return a `Response`.
+        (&Method::POST, "/echo/reversed") => {
+            let whole_body = hyper::body::to_bytes(req.into_body()).await?;
+
+            let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
+            Ok(Response::new(Body::from(reversed_body)))
+        }
+
+        // Return the 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
+
+    let server = Server::bind(&addr).serve(service);
+
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    Ok(())
+}
+
+// extern crate image;
+
+// use image::GenericImageView;
+
+// fn main() {
+//     let img = image::open("img/input/progressive.jpg").unwrap();
+//     println!("dimensions {:?}", img.dimensions());
+//     println!("{:?}", img.color());
+//     img
+//     .resize(160, 90, image::imageops::Lanczos3)
+//     .save("img/output/progressive.jpg")
+//     .unwrap();
+// }
